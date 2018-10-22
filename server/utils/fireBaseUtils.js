834dc2e83633289b284a7792c08184fe0a58ad48
Muhammad Usman <muhammadusman1794@gmail.com>
2018-10-22 17:42:38 +0500

    refactoring

--- server/utils/fireBaseUtils.js
+++ server/utils/fireBaseUtils.js
@@ -3,7 +3,18 @@ import * as admin from 'firebase-admin'
 function intializeFirebase() {
     const serviceAccount = require('../../config/firebaseConfig/bincee-67ec6-firebase-adminsdk-k1zs9-3b3e1e9c89.json')
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: 'https://bincee-67ec6.firebaseio.com',
+        credential: admin.credential.cert({
+            type: 'service_account',
+            project_id: process.env.PROJECT_ID,
+            private_key_id: process.env.PRIVATE_KEY_ID,
+            private_key: process.env.PRIVATE_KEY,
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+            token_uri: 'https://oauth2.googleapis.com/token',
+            auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+            client_x509_cert_url: process.env.CLIENT_CERT_URL,
+        }),
+        databaseURL: process.env.DATABASE_URL,
     })
 }
