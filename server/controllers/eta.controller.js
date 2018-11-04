181f7e3f8f2d77f82cae2607c592cb2012be266a
Muhammad Usman <muhammadusman1794@gmail.com>
2018-11-05 01:04:24 +0500

    route utility

--- server/controllers/eta.controller.js
+++ server/controllers/eta.controller.js
@@ -6,12 +6,23 @@ import flow from 'lodash/fp/flow'
 import split from 'lodash/fp/split'
 
 //src
-import { getDistanceMatrix } from '../utils/etaUtils'
+import { getDistanceMatrix, getDirection } from '../utils/etaUtils'
 
 function getEta(req, res) {
-    const { from, to } = getOr({}, 'body')(req)
+    const { from, to, waypoints } = getOr({}, 'body')(req)
 
-    return getDistanceMatrix(JSON.parse(from), JSON.parse(to)).then(resETA => {
+    // return getDistanceMatrix(JSON.parse(from), JSON.parse(to)).then(resETA => {
+    //     if (resETA) {
+    //         return res.status(200).json(resETA)
+    //     }
+    //     res.status(404).json({ message: 'ETA Not Found' })
+    // })
+
+    return getDirection(
+        JSON.parse(from),
+        JSON.parse(to),
+        JSON.parse(waypoints),
+    ).then(resETA => {
         if (resETA) {
             return res.status(200).json(resETA)
         }
--- server/utils/etaUtils.js
+++ server/utils/etaUtils.js
@@ -3,9 +3,11 @@ import join from 'lodash/join'
 import size from 'lodash/size'
 import map from 'lodash/fp/map'
 import fetch from 'node-fetch'
-import {} from '@google/maps'
+
 const distanceBaseUrl =
     'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
+const directionBaseUrl =
+    'https://maps.googleapis.com/maps/api/directions/json?units=imperial'
 
 export function getDistanceMatrix(origins, destinations) {
     const originQuery = generateRequestParamsFromFilters(origins)
@@ -16,7 +18,16 @@ export function getDistanceMatrix(origins, destinations) {
 
     return fetch(distanceMatrixUrl).then(res => res.json())
 }
+export function getDirection(origin, destination, waypoints) {
+    const originQuery = generateRequestParamsFromFilters([origin])
+    const destinationQuery = generateRequestParamsFromFilters([destination])
+    const waypointsQuery = generateRequestParamsFromFilters(waypoints)
+    const directionUrl = `${directionBaseUrl}&origin=${originQuery}&destination=${destinationQuery}&waypoints=${waypointsQuery}&key=${
+        process.env.ETA_API_KEY
+    }`
 
+    return fetch(directionUrl).then(res => res.json())
+}
 function generateRequestParamsFromFilters(locations) {
     if (locations.length === 1) {
         const { lat, lng } = locations[0]
@@ -27,6 +38,6 @@ function generateRequestParamsFromFilters(locations) {
     return `${encodeURIComponent(join(locationArray, '|'))}`
 }
 
-function find(originIndex, destinationindex, data) {
-    return [(originIndex - 1) * size(data)] + destinationIndex
-}
+// function find(originIndex, destinationindex, data) {
+//     return [(originIndex - 1) * size(data)] + destinationIndex
+// }
