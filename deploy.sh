843021ae505cafdb403bed8b4541f94f4cec5ae4
sadainiqrar <sadain.abbasi@gmail.com>
2019-01-05 02:50:20 +0500

    kill

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ echo "1/2) Stopping existing node process ..."
 forever stop apis
 
 echo "2/2) Starting node process ..."
+echo "3/7) Starting node process ..."
+sudo lsof -t -i tcp:3000 -s tcp:listen | sudo xargs kill
 forever start --uid "apis" -a -c node dist/index.js
 
 echo "Deployment complete"
