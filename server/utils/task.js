72b99460757aac8dc66886c316ef05a2cb623009
sadainiqrar <sadain.abbasi@gmail.com>
2019-02-11 17:55:24 +0500

    timezone dynamic

--- server/utils/task.js
+++ server/utils/task.js
@@ -90,7 +90,7 @@ export function morningTask() {
 }
 
 export function eveningTask() {
-    const date = moment().tz('Asia/Baghdad')
+    const date = moment()
     const currentTime = date
         .set({ second: 0, millisecond: 0 })
         .format('HH:mm:ss')
