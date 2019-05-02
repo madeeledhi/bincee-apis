5d657fa9ef7b74dbd90486acdca1eacbb8c4180d
sadainiqrar <sadain.abbasi@gmail.com>
2019-05-02 23:36:25 +0500

    notification fixes

--- server/controllers/school.controller.js
+++ server/controllers/school.controller.js
@@ -24,7 +24,7 @@ import {
     destroy,
     update,
     sendNotification,
-    sendBulkNotifications,
+    sendShiftNotification,
     sendEmail,
     getFBData,
     sendSMS,
--- server/utils/notificationUtils.js
+++ server/utils/notificationUtils.js
@@ -2,50 +2,46 @@
 import fetch from 'node-fetch'
 import fcmNode from 'fcm-node'
 
-const notificationUrl =
-    'https://fcm.googleapis.com/v1/projects/bincee-67ec6/messages:send'
+const notificationUrl = 'https://fcm.googleapis.com/fcm/send'
 const token = process.env.FCM_TOKEN || ''
+
+const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `key=${token}`,
+}
 
 export function sendNotification(topic, notification, data) {
     const message = {
-        to: topic,
+        topic: `/topics/${topic}`,
         notification,
         data,
     }
-    const fcm = new fcmNode(token)
-
-    return new Promise((resolve, reject) => {
-        fcm.send(message, function(err, response) {
-            if (err) {
-                console.log('Something has gone wrong!')
-                reject(err)
-            } else {
-                console.log('Successfully sent with response: ', response)
-                resolve(response)
-            }
+    return fetch(notificationUrl, { method: 'POST', headers: headers })
+        .then(response => {
+            console.log('Successfully sent with response: ', response)
+            return response
         })
-    })
+        .catch(err => {
+            console.log('Successfully sent with response: ', err)
+            return err
+        })
 }
 
 export function sendShiftNotification(id, notification, data) {
     const message = {
-        to: `parent_${id}`,
+        topic: `/topics/parent_${id}`,
         notification,
         data,
     }
-    const fcm = new fcmNode(token)
-
-    return new Promise((resolve, reject) => {
-        fcm.send(message, function(err, response) {
-            if (err) {
-                console.log('Something has gone wrong!')
-                reject(err)
-            } else {
-                console.log('Successfully sent with response: ', response)
-                resolve(response)
-            }
+    return fetch(notificationUrl, { method: 'POST', headers: headers })
+        .then(response => {
+            console.log('Successfully sent with response: ', response)
+            return response
         })
-    })
+        .catch(err => {
+            console.log('Successfully sent with response: ', err)
+            return err
+        })
 }
 
 export function sendBulkNotifications(id, notification, data) {
@@ -54,17 +50,13 @@ export function sendBulkNotifications(id, notification, data) {
         notification,
         data,
     }
-    const fcm = new fcmNode(token)
-
-    return new Promise((resolve, reject) => {
-        fcm.send(message, function(err, response) {
-            if (err) {
-                console.log('Something has gone wrong!')
-                reject(err)
-            } else {
-                console.log('Successfully sent with response: ', response)
-                resolve(response)
-            }
+    return fetch(notificationUrl, { method: 'POST', headers: headers })
+        .then(response => {
+            console.log('Successfully sent with response: ', response)
+            return response
         })
-    })
+        .catch(err => {
+            console.log('Successfully sent with response: ', err)
+            return err
+        })
 }
